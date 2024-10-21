92697bbe3ba5c99d24714a0a5c8d5baf9004f1b2
Yanelli Gloria <95405769+yanelli-gloria@users.noreply.github.com>
2024-10-20 23:57:04 -0700

    Update script.js

--- ygloria-lab3/quarterly-goals/script.js
+++ ygloria-lab3/quarterly-goals/script.js
@@ -1,13 +1,15 @@
 document.getElementById('add-goal-btn').onclick = function() {
     const goal = prompt("Enter a new quarter goal:");
     const hashtag = prompt("Enter a good hashtag:");
-    if (goal && hashtag) {
+    const completionStatus = prompt("Enter a goal-completion status:");
+
+    if (goal && hashtag && completionStatus) {
         const goalList = document.getElementById('goal-list');
-        const newGoal = `
+        goalList.insertAdjacentHTML('beforeend', `
             <li>
                 <input type="checkbox"> ${goal} 
-                <span class="tag new">${hashtag}</span>
-            </li>`;
-        goalList.insertAdjacentHTML('beforeend', newGoal);
+                <span class="tag new">${hashtag}</span> 
+                <span>(${completionStatus})</span>
+            </li>`);
     }
 };
